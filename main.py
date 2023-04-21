2c00ff5fa9bddca04748e76484c3b91cf23900b5
Ali <Mr.Ali.Fotouhi@gmail.com>
2023-04-21 21:08:08 +0430

    Add ArgumentParser

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+from argparse import ArgumentParser
+
 from compressor import ImageCompressor
 
 
 def main() -> None:
-    src, dest = "src/", "dest/"
-    quality = 90
+    parser = ArgumentParser(
+        "Image Compressor",
+        "Simple script to compress your images"
+    )
+    parser.add_argument("-s", "--source", type=str, required=True, help="Source directory")
+    parser.add_argument("-d", "--destination", type=str, required=True, help="Destination directory")
+    parser.add_argument("-o", "--optimize", type=bool, help="Optimize images", default=True)
+    parser.add_argument("-q", "--quality", type=int, help="Images quality", default=90)
+    parser.add_argument("-w", "--workers", type=int, help="Max workers count", default=4)
+    args = parser.parse_args()
 
-    obj = ImageCompressor(src, dest, True, quality)
-    result = obj.compress_images()
+    comp = ImageCompressor(args.source, args.destination, args.optimize, args.quality)
+    result = comp.compress_images(args.workers)
 
     print(f"{result.count} Image{'s' if result.count > 1 else ''} were compressed in {round(result.time, 2)}"
           f" second{'s' if result.time > 1 else ''}")
