14d5644ff26d0e1d7b3a7b080d9a20f4179fe45e
Ali <Mr.Ali.Fotouhi@gmail.com>
2023-04-23 00:16:02 +0430

    Select paths on button clicked

--- gui/app.py
+++ gui/app.py
@@ -1,6 +1,6 @@
 import sys
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QFileDialog
 from qt_material import apply_stylesheet
 
 from ui import MainWidget
@@ -9,15 +9,21 @@ from ui import MainWidget
 class App(MainWidget):
     def __init__(self) -> None:
         super().__init__()
+        self._src_dir = None
+        self._dest_dir = None
 
     def on_compress_btn_click(self):
         pass
 
     def on_src_btn_click(self):
-        pass
+        if (path := QFileDialog.getExistingDirectory()) is not None:
+            self._src_dir = path
+            self._te_source_dir.setText(path)
 
     def on_dest_btn_click(self):
-        pass
+        if (path := QFileDialog.getExistingDirectory()) is not None:
+            self._dest_dir = path
+            self._te_dest_dir.setText(path)
 
 
 if __name__ == '__main__':
